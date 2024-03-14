ff4ac74bd9802b5c2c0278479320108373351ae2
unknown <bernhard.dev@gmail.com>
2024-03-14 16:22:34 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import 'colors';
 import mongoSanitize from 'express-mongo-sanitize';
-import { xss } from 'express-xss-sanitizer';
+//import { xss } from 'express-xss-sanitizer';
 import helmet from 'helmet';
-import hpp from 'hpp';
-import limiter from './lib/rate-limit';
+//import hpp from 'hpp';
+//import limiter from './lib/rate-limit';
 import cards from './routes/cards';
 import archetypes from './routes/archetypes';
 import errorHandler from './custom-middleware/error';
@@ -16,16 +16,16 @@ import cors from 'cors';
 const app = express();
 
 //Load env variables
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '../.env' });
 
 connectDB(); //Connect to database
 app.use(express.json()); // Body parser
 app.use(cors()); // Body parser
 app.use(helmet()); //Set security headers
 app.use(mongoSanitize()); // Sanitize data
-app.use(hpp()); //Prevent http param pollution
-//app.use(limiter); // Apply the rate limiting middleware to all requests.
-//app.use(xss()); //Prvent XSS attacks
+// app.use(hpp()); //Prevent http param pollution
+// app.use(limiter); // Apply the rate limiting middleware to all requests.
+// app.use(xss()); //Prvent XSS attacks
 
 //Dev logging middleware
 //app.use(logger); /*Custom middleware */
@@ -43,9 +43,9 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.PORT}`.yellow.bold)
-);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.PORT}`.yellow.bold);
+});
 
 process.on('unhandledRejection', (error: any, promise) => {
   console.log(`Unhanlded Rejection - Error : ${error.message}`.red.bold);
